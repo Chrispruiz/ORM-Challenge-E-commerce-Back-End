f7a41e357cf8d75f8d5479dab79bc987dcc9ca10
Chris Ruiz <chrispruiz@att.net>
2021-05-12 23:08:44 -0500

    fixed category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -12,17 +12,11 @@ router.get('/', (req, res) => {
       attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
     }
   })
-  .then(dbCatData => {
-    if(!dbCatData) {
-      res.status(404).json({message: 'No categories found'});
-      return;
-    }
-    res.json(dbCatData);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err)
-  });
+  .then(categoryData => res.json(categoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
@@ -37,47 +31,45 @@ router.get('/:id', (req, res) => {
       attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
     }
   })
-    .then(dbCatData => {
-      if(!dbCatData) {
-        res.status(404).json({message: 'No categories found'});
-        return;
-      }
-      res.json(dbCatData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err)
-    });
+  .then(categoryData => res.json(categoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
   Category.create({
     category_name: req.body.category_name
   })
-    .then(dbCatData => res.json(dbCatData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-router.put('/:id', (req, res) => {
-  Category.update(req.body, {
-    where: {
-      id: req.params.id
-    }
-  })
-  .then(dbCatData => {
-    if (!dbCatData) {
-      res.status(404).json({message:'No categories exist with this id'});
-      return;
-    }
-    res.json(dbCatData);
-  })
+  .then(categoryData => res.json(categoryData))
   .catch(err => {
     console.log(err);
     res.status(500).json(err);
   });
+});
+
+router.put('/:id', (req, res) => {
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(categoryData => {
+      if (!categoryData) {
+        res.status(404).json({ message: 'No categories exist with that id' });
+        return;
+      }
+      res.json(categoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
@@ -87,12 +79,12 @@ router.delete('/:id', (req, res) => {
       id: req.params.id
     }
   })
-    .then(dbCatData => {
-      if (!dbCatData){
+    .then(categoryData => {
+      if (!categoryData){
         res.status(404).json({message: 'No categories exist with that id'});
         return;
       }
-      res.json(dbCatData);
+      res.json(categoryData);
     })
     .catch(err => {
       console.log(err);
